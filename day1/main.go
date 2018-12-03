b4ae8fdae7316cdb87abddd422548f6d7e535857
sebastienD <sebastien.descamps@gmail.com>
2018-12-03 14:24:26 +0100

    🎉 init

---
+++ day1/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+}
